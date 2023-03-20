e76ac2ef5f492f25fcc5077d47d587dabbe527df
BTL86 <phamlehuy52@gmail.com>
2023-03-20 16:29:55 +0700

    switch to random image generation

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./mongodb/connect.js";
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
 
-import postRoutes from "./routes/postRoutes.js";
-import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 
-app.use("/api/v1/posts", postRoutes);
-app.use("/api/v1/dalle", dalleRoutes);
+app.use('/api/v1/posts', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
 
-app.get("/", async (req, res) => {
-	res.send("Hello from DALL-E!");
+app.get('/', async (req, res) => {
+	res.send('Hello from DALL-E!');
 });
 
 const startServer = async () => {
@@ -25,9 +25,7 @@ const startServer = async () => {
 	} catch (error) {
 		console.log(error);
 	}
-	app.listen(8080, () =>
-		console.log("Server started on port  http://localhost:8080")
-	);
+	app.listen(8080, () => console.log('Server started on port  http://localhost:8080'));
 };
 
 startServer();
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,7 +1,47 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import express, { response } from 'express';
+import * as dotenv from 'dotenv';
+import fetch from 'node-fetch';
+// import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
+
+function streamToString(stream, encoding = 'base64') {
+	const chunks = [];
+	return new Promise((resolve, reject) => {
+		stream.on('data', chunk => chunks.push(Buffer.from(chunk)));
+		stream.on('error', err => reject(err));
+		stream.on('end', () => resolve(Buffer.concat(chunks).toString(encoding)));
+	});
+}
+// const configuration = new
+//
+router.route('/').get((req, res) => {
+	res.send('hello from dalle');
+});
+
+const _getRandomInt = maxx => Math.floor(Math.random() * maxx);
+
+router.route('/').post(async (req, res) => {
+	try {
+		const { prompt } = req.body;
+		// console.log({ prompt });
+		// const { id, output_url } = await deepai.callStandardApi('text2img', {
+		// 	text: prompt
+		// });
+
+		const response = await fetch(`https://picsum.photos/${_getRandomInt(1000)}/${_getRandomInt(1000)}`, { method: 'GET' });
+
+		// console.log({ response });
+		if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+		const data = await streamToString(response.body);
+		const contentType = response.headers['content-type'];
+		res.status(200).json({ photo: `data:${contentType ? contentType : 'image/png'};base64,${data}` });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send(error);
+	}
+});
+
+export default router;
